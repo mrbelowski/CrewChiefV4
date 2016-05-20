cb79f80904a5f8516454127763eb81a5c8628a6c
mrbelowski <jim.britton@yahoo.co.uk>
2016-05-20 22:14:45 +0100

    added lmp3 (aquilla) to new tyre model heating

--- CrewChiefV4/CarData.cs
+++ CrewChiefV4/CarData.cs
@@ -90,7 +90,7 @@ namespace CrewChiefV4
         }
 
         public static CarClassEnum[] r3eNewTyreModelClasses = new CarClassEnum[] {
-            CarClassEnum.GT3 ,CarClassEnum.GT4 ,CarClassEnum.LMP1 ,CarClassEnum.LMP2 ,CarClassEnum.GROUP5 ,CarClassEnum.GTO ,CarClassEnum.F2 ,CarClassEnum.F4 ,
+            CarClassEnum.GT3 ,CarClassEnum.GT4 ,CarClassEnum.LMP1 ,CarClassEnum.LMP2 ,CarClassEnum.LMP3, CarClassEnum.GROUP5 ,CarClassEnum.GTO ,CarClassEnum.F2 ,CarClassEnum.F4 ,
             CarClassEnum.FF ,CarClassEnum.TC1 ,CarClassEnum.AUDI_TT_CUP ,CarClassEnum.DTM_2015 ,CarClassEnum.NSU_TT};
 
         public class CarClass
