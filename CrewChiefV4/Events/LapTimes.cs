78892add40bc311aa8d6a371d0ea1bddae1631ad
Jim Britton <jim.britton@yahoo.co.uk>
2016-05-25 08:15:02 +0100

    trivial performance tweak

--- CrewChiefV4/Events/LapTimes.cs
+++ CrewChiefV4/Events/LapTimes.cs
@@ -1356,13 +1356,15 @@ namespace CrewChiefV4.Events
         }
 
         public static Boolean nearlyEqual(float a, float b, float epsilon) {
+            if (a == b)
+            {
+                return true;
+            }
             float absA = Math.Abs(a);
             float absB = Math.Abs(b);
             float diff = Math.Abs(a - b);
 
-            if (a == b) { // shortcut, handles infinities
-                return true;
-            } else if (a == 0 || b == 0 || diff < float.Epsilon) {
+            if (a == 0 || b == 0 || diff < float.Epsilon) {
                 // a or b is zero or both are extremely close to it
                 // relative error is less meaningful here
                 return diff < (epsilon * float.Epsilon);
