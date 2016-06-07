be7572e4fdeff787941209fdd380b6527577984d
Dan Allongo <daniel.s.allongo@gmail.com>
2016-06-06 22:28:55 -0600

    initial framework for rFactor 1 support

---
+++ CrewChiefV4/RF1/RF1Data.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CrewChiefV4.rFactor1
+{
+    class rFactor1Constant
+    {
+        public const string SharedMemoryName = "$rFactorShared$";
+
+        public enum rfGamePhase
+        {
+			garage = 0,
+			warmUp = 1,
+			gridWalk = 2,
+			formation = 3,
+			countdown = 4,
+			greenFlag = 5,
+			fullCourseYellow = 6,
+			sessionStopped = 7,
+			sessionOver = 8
+        }
+
+        public enum rfYellowFlagState
+        {
+			invalid = -1,
+			noFlag = 0,
+			pending = 1,
+			pitClosed = 2,
+			pitLeadLap = 3,
+			pitOpen = 4,
+			lastLap = 5,
+			resume = 6,
+			raceHalt = 7
+        }
+
+        public enum rfSurfaceType
+        {
+			dry = 0,
+			wet = 1,
+			grass = 2,
+			dirt = 3,
+			gravel = 4,
+			kerb = 5
+        }
+
+        public enum rfSector
+        {
+			sector3 = 0,
+			sector1 = 1,
+			sector2 = 2
+        }
+
+        public enum rfFinishStatus
+        {
+			none = 0,
+			finished = 1,
+			dnf = 2,
+			dq = 3
+        }
+
+        public enum rfControl
+        {
+			nobody = -1,
+			player = 0,
+			ai = 1,
+			remote = 2,
+			replay = 3
+        }
+
+        public enum rfWheelIndex
+        {
+			frontLeft = 0,
+			frontRight = 1,
+			rearLeft = 2,
+			rearRight = 3
+        }
+    }
+
+    namespace rFactor1Data
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct rfVec3
+        {
+            public Single x;
+            public Single y;
+            public Single z;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct rfWheel
+        {
+            public Single rotation;
+            public Single suspensionDeflection;
+            public Single rideHeight;
+            public Single tireLoad;
+            public Single lateralForce;
+            public Single gripFract;
+            public Single brakeTemp;
+            public Single pressure;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 3)]
+            public Single[] temperature;
+            public Single wear;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 16)]
+            public byte[] terrainName;
+            public byte surfaceType;
+            public bool flat;
+            public bool detached;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct rfVehicleInfo
+        {
+        	[MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 32)]
+            public byte[] driverName;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 64)]
+            public byte[] vehicleName;
+            public Int16 totalLaps;
+            public sbyte sector;
+            public sbyte finishStatus;
+            public Single lapDist;
+            public Single pathLateral;
+            public Single trackEdge;
+
+            public Single bestSector1;
+            public Single bestSector2;
+            public Single bestLapTime;
+            public Single lastSector1;
+            public Single lastSector2;
+            public Single lastLapTime;
+            public Single curSector1;
+            public Single curSector2;
+
+            public Int16 numPitstops;
+            public Int16 numPenalties;
+            public bool isPlayer;
+            public sbyte control;
+            public bool inPits;
+            public byte place;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 32)]
+            public byte[] vehicleClass;
+
+            public Single timeBehindNext;
+            public Int64 lapsBehindNext;
+            public Single timeBehindLeader;
+            public Int64 lapsBehindLeader;
+            public Single lapStartET;
+
+            public rfVec3 pos;
+            public rfVec3 localVel;
+            public rfVec3 localAccel;
+
+            public rfVec3 oriX;
+            public rfVec3 oriY;
+            public rfVec3 oriZ;
+            public rfVec3 localRot;
+            public rfVec3 localRotAccel;
+
+            public Single speed;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct rfShared
+        {
+            public Single deltaTime;
+            public Int64 lapNumber;
+            public Single lapStartET;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 64)]
+            public byte[] vehicleName;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 64)]
+            public byte[] trackName;
+
+            public rfVec3 pos;
+            public rfVec3 localVel;
+            public rfVec3 localAccel;
+
+            public rfVec3 oriX;
+            public rfVec3 oriY;
+            public rfVec3 oriZ;
+            public rfVec3 localRot;
+            public rfVec3 localRotAccel;
+
+            public Single speed;
+
+            public Int64 gear;
+            public Single engineRPM;
+            public Single engineWaterTemp;
+            public Single engineOilTemp;
+            public Single clutchRPM;
+
+            public Single unfilteredThrottle;
+            public Single unfilteredBrake;
+            public Single unfilteredSteering;
+            public Single unfilteredClutch;
+
+            public Single steeringArmForce;
+            public Single fuel;
+            public Single engineMaxRPM;
+            public byte scheduledStops;
+            public bool overheating;
+            public bool detached;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 8)]
+            public byte[] dentSeverity;
+            public Single lastImpactET;
+            public Single lastImpactMagnitude;
+            public rfVec3 lastImpactPos;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 4)]
+            public rfWheel[] wheel;
+
+            public Int64 session;
+            public Single currentET;
+            public Single endET;
+            public Int64 maxLaps;
+            public Single lapDist;
+
+            public Int64 numVehicles;
+
+            public byte gamePhase;
+
+            public sbyte yellowFlagState;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 3)]
+            public sbyte[] sectorFlag;
+            public byte startLight;
+            public byte numRedLights;
+            public bool inRealtime;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst =32)]
+            public byte[] playerName;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 64)]
+            public byte[] plrFileName;
+
+            public Single ambientTemp;
+            public Single trackTemp;
+            public rfVec3 wind;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 128)]
+            public rfVehicleInfo[] vehicle;
+        }
+    }
+}
---
+++ CrewChiefV4/RF1/RF1SharedMemoryReader.cs
@@ -0,0 +1,164 @@
+﻿using CrewChiefV4.rFactor1;
+using CrewChiefV4.rFactor1.rFactor1Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CrewChiefV4.rFactor1
+{
+    public class RF1SharedMemoryReader : GameDataReader
+    {
+        private MemoryMappedFile memoryMappedFile;
+        private GCHandle handle;
+        private int sharedmemorysize;
+        private byte[] sharedMemoryReadBuffer;
+        private Boolean initialised = false;
+        private List<RF1StructWrapper> dataToDump;
+        private RF1StructWrapper[] dataReadFromFile = null;
+        private int dataReadFromFileIndex = 0;
+        private String lastReadFileName = null;
+
+        public class RF1StructWrapper
+        {
+            public long ticksWhenRead;
+            public rfShared data;
+        }
+
+        public override void DumpRawGameData()
+        {
+            if (dumpToFile && dataToDump != null && dataToDump.Count > 0 && filenameToDump != null)
+            {
+                foreach (RF1StructWrapper wrapper in dataToDump)
+                {
+                    wrapper.data.vehicle = getPopulatedVehicleInfoArray(wrapper.data.vehicle);
+                }
+                SerializeObject(dataToDump.ToArray<RF1StructWrapper>(), filenameToDump);
+            }
+        }
+
+        public override void ResetGameDataFromFile()
+        {
+            dataReadFromFileIndex = 0;
+        }
+
+        public override Object ReadGameDataFromFile(String filename)
+        {
+            if (dataReadFromFile == null || filename != lastReadFileName)
+            {
+                dataReadFromFileIndex = 0;
+                dataReadFromFile = DeSerializeObject<RF1StructWrapper[]>(dataFilesPath + filename);
+                lastReadFileName = filename;
+            }
+            if (dataReadFromFile != null && dataReadFromFile.Length > dataReadFromFileIndex)
+            {
+                RF1StructWrapper structWrapperData = dataReadFromFile[dataReadFromFileIndex];
+                dataReadFromFileIndex++;
+                return structWrapperData;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        protected override Boolean InitialiseInternal()
+        {
+            if (dumpToFile)
+            {
+                dataToDump = new List<RF1StructWrapper>();
+            }
+            lock (this)
+            {
+                if (!initialised)
+                {
+                    try
+                    {
+                        memoryMappedFile = MemoryMappedFile.OpenExisting(rFactor1Constant.SharedMemoryName);
+                        sharedmemorysize = Marshal.SizeOf(typeof(rfShared));
+                        sharedMemoryReadBuffer = new byte[sharedmemorysize];
+                        initialised = true;
+                        Console.WriteLine("Initialised rFactor 1 shared memory");
+                    }
+                    catch (Exception)
+                    {
+                        initialised = false;
+                    }
+                }
+                return initialised;
+            }
+        }
+
+        public override Object ReadGameData(Boolean forSpotter)
+        {
+            lock (this)
+            {
+                rfShared _rf1apistruct = new rfShared();
+                if (!initialised)
+                {
+                    if (!InitialiseInternal())
+                    {
+                        throw new GameDataReadException("Failed to initialise shared memory");
+                    }
+                }
+                try
+                {
+                    using (var sharedMemoryStreamView = memoryMappedFile.CreateViewStream())
+                    {
+                        BinaryReader _SharedMemoryStream = new BinaryReader(sharedMemoryStreamView);
+                        sharedMemoryReadBuffer = _SharedMemoryStream.ReadBytes(sharedmemorysize);
+                        handle = GCHandle.Alloc(sharedMemoryReadBuffer, GCHandleType.Pinned);
+                        _rf1apistruct = (rfShared)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(rfShared));
+                        handle.Free();
+                    }
+                    RF1StructWrapper structWrapper = new RF1StructWrapper();
+                    structWrapper.ticksWhenRead = DateTime.Now.Ticks;
+                    structWrapper.data = _rf1apistruct;
+                    if (!forSpotter && dumpToFile && dataToDump != null)
+                    {
+                        dataToDump.Add(structWrapper);
+                    }
+                    return structWrapper;
+                }
+                catch (Exception ex)
+                {
+                    throw new GameDataReadException(ex.Message, ex);
+                }
+            }
+        }
+
+        private rfVehicleInfo[] getPopulatedVehicleInfoArray(rfVehicleInfo[] raw)
+        {
+            List<rfVehicleInfo> populated = new List<rfVehicleInfo>();
+            foreach (rfVehicleInfo rawData in raw)
+            {
+                if (rawData.place > 0)
+                {
+                    populated.Add(rawData);
+                }
+            }
+            if (populated.Count == 0)
+            {
+                populated.Add(raw[0]);
+            }
+            return populated.ToArray();
+        }
+
+        public override void Dispose()
+        {
+            if (memoryMappedFile != null)
+            {
+                try
+                {
+                    memoryMappedFile.Dispose();
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
