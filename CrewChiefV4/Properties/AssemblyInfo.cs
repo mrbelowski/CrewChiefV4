40bdfa7d2cc248e3f736f44cb804102e5840507e
Dan Allongo <daniel.s.allongo@gmail.com>
2016-06-17 00:59:23 -0600

    increment version, add readme entry

--- CrewChiefV4/Properties/AssemblyInfo.cs
+++ CrewChiefV4/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CrewChiefV4")]
-[assembly: AssemblyDescription("RaceRoom & PCars race engineer")]
+[assembly: AssemblyDescription("RaceRoom, PCars, and rFactor race engineer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Britton IT Ltd")]
 [assembly: AssemblyProduct("CrewChiefV4")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.2.1.8")]
+[assembly: AssemblyVersion("4.3.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
