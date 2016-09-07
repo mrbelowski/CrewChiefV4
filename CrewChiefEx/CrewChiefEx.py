c7838ab055696ddcda4c151ed7455f3b6d182d3d
Sparten <sportcorp@hotmail.com>
2016-09-07 13:07:54 +0200

    Cleaned up python for release

--- CrewChiefEx/CrewChiefEx.py
+++ CrewChiefEx/CrewChiefEx.py
@@ -4,7 +4,7 @@ import acsys
 import sys
 import math, os.path
 import platform
-import mmap
+
 # import libraries
 if platform.architecture()[0] == "64bit":
     sysdir=os.path.dirname(__file__)+'/stdlib64'
@@ -14,43 +14,24 @@ sys.path.insert(0, sysdir)
 os.environ['PATH'] = os.environ['PATH'] + ";."
 
 
-
-try:
-  import configparser
-except ImportError as e:
-  ac.log("{}".format(e))
-
-
 from sim_info import SimInfo
 from sim_info import SPageFileCrewChief
 from sim_info import acsVehicleInfo
 
-maxcars = 64
-drivers = []
-player = 0
-maxSlotId = 0
 siminfo = SimInfo()
-driverName = ""
 
-l_lapcount=0
-l_driver=0
-l_drivers=0
-l_flag=0
+timer = 0
 
-lapcount=0
-state = ''
-timer1 = 0
 def splineToDistanceRoundTrack(tracklen, splinepos):
 
     return (splinepos * tracklen)
 
 
 def updateSharedMemory():
-    global siminfo,maxSlotId,state
+    global siminfo
     sharedmem = siminfo.getsharedmem()
     siminfo.update()
     iscountdown = 0
-
     sharedmem.numVehicles = ac.getCarsCount()
     sharedmem.focusVehicle = ac.getFocusedCar()
     tracklenght = siminfo.static.trackSPlineLength
@@ -107,67 +88,20 @@ def updateSharedMemory():
             
 
 def acMain(ac_version):
-  global appWindow,l_lapcount,l_driver,l_drivers,l_flag
+  global appWindow
   appWindow = ac.newApp("CrewChiefEx")
   ac.setTitle(appWindow, "CrewChiefEx")
 
-  ac.setSize(appWindow, 400, 200)
+  ac.setSize(appWindow, 300, 40)
 
   ac.log("CrewChief Was Here! damage report ?")
   ac.console("CrewChief Was Here! damage report ?")
 
-  l_lapcount = ac.addLabel(appWindow, "");
-  l_driver = ac.addLabel(appWindow, "");
-  l_drivers = ac.addLabel(appWindow, "");
-  l_flag = ac.addLabel(appWindow, "");
-  ac.setPosition(l_lapcount, 3, 30)
-  ac.setPosition(l_driver, 3, 42)
-  ac.setPosition(l_drivers, 3, 54)
-  ac.setPosition(l_flag, 3, 66)
-
   return "CrewChiefEx"
 
 def acUpdate(deltaT):
-    global siminfo
-    global l_lapcount, lapcount,l_driver,l_drivers,maxSlotId,l_flag, timer1
-    
-    timer1 += deltaT
-    if timer1 > 0.05:
+    global timer
+    timer += deltaT
+    if timer > 0.05:
         updateSharedMemory()
-        timer1 = 0
-    
-    #sharedmem = siminfo.getsharedmem()
-    
-    #sessionTimeValue = siminfo.graphics.sessionTimeLeft
-    #ValueSeconds = (sessionTimeValue / 1000) % 60
-    #ValueMinutes = (sessionTimeValue // 1000) // 60
-
-    #isOnline = len(ac.getServerName())
-    #iscountdown = 0
-    #sessiontype = siminfo.graphics.session
-
-
-    #if isOnline > 0:
-    #    if sessiontype == 2 or sessiontype == 5 or sessiontype == 6: 
-    #        if ValueMinutes >= 0.0 and ValueSeconds >= 0.1:
-    #            iscountdown = 1
-    #elif int(ValueMinutes) == 30 and ValueSeconds < 9.999:
-    #    iscountdown = 1
-
-
-    #ac.setText(l_lapcount,"servername {}".format( ac.getServerName() ))
-
-    #tracklenght = siminfo.static.trackSPlineLength
-    #distance = splineToDistanceRoundTrack(tracklenght, ac.getCarState(0, acsys.CS.NormalizedSplinePosition) )
-    #ac.setText(l_driver, "timeleft {:.0f}:{:05.2f}".format(ValueMinutes, ValueSeconds) )  
-    #ac.setText(l_drivers, "currentSectorIndex {}".format( siminfo.graphics.currentSectorIndex ) )
-    #ac.setText(l_flag, "countDown {}".format( iscountdown ))
-
-        
-    
-    
-
-
-
-
-
+        timer = 0
