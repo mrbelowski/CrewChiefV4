00e8379cc33386f256f8c1bc0f227503e31fabff
mrbelowski <jim.britton@yahoo.co.uk>
2017-02-12 18:04:29 +0000

    reworked some of the English number reading

--- CrewChiefV4/UserInterface/MainWindow.cs
+++ CrewChiefV4/UserInterface/MainWindow.cs
@@ -63,7 +63,7 @@ namespace CrewChiefV4
         private float currentVolume = -1;
                 
         private void FormMain_Load(object sender, EventArgs e)
-        {
+        {            
             // Some update test code - uncomment this to allow the app to process an update .zip file in the root of the sound pack
             /*
             ZipFile.ExtractToDirectory(AudioPlayer.soundFilesPath + @"\" + soundPackTempFileName, AudioPlayer.soundFilesPath + @"\sounds_temp");
