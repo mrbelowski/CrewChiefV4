974056d8044cfc4da1c9404e558fd04a9f17e5bc
mrbelowski <jim.britton@yahoo.co.uk>
2017-03-07 10:49:55 +0000

    sort games list alphabetically for the hard-of-thinking

--- CrewChiefV4/GameDefinition.cs
+++ CrewChiefV4/GameDefinition.cs
@@ -12,29 +12,29 @@ namespace CrewChiefV4
     }
     public class GameDefinition
     {
-        public static GameDefinition pCars64Bit = new GameDefinition(GameEnum.PCARS_64BIT, Configuration.getUIString("pcars_64_bit"), "pCARS64",
+        public static GameDefinition pCars64Bit = new GameDefinition(GameEnum.PCARS_64BIT, "pcars_64_bit", "pCARS64",
             "CrewChiefV4.PCars.PCarsSpotterv2", "pcars64_launch_exe", "pcars64_launch_params", "launch_pcars");
-        public static GameDefinition pCars32Bit = new GameDefinition(GameEnum.PCARS_32BIT, Configuration.getUIString("pcars_32_bit"), "pCARS",
+        public static GameDefinition pCars32Bit = new GameDefinition(GameEnum.PCARS_32BIT, "pcars_32_bit", "pCARS",
             "CrewChiefV4.PCars.PCarsSpotterv2", "pcars32_launch_exe", "pcars32_launch_params", "launch_pcars");
-        public static GameDefinition raceRoom = new GameDefinition(GameEnum.RACE_ROOM, Configuration.getUIString("race_room"), "RRRE", "CrewChiefV4.RaceRoom.R3ESpotterv2",
+        public static GameDefinition raceRoom = new GameDefinition(GameEnum.RACE_ROOM, "race_room", "RRRE", "CrewChiefV4.RaceRoom.R3ESpotterv2",
             "r3e_launch_exe", "r3e_launch_params", "launch_raceroom");
-        public static GameDefinition pCarsNetwork = new GameDefinition(GameEnum.PCARS_NETWORK, Configuration.getUIString("pcars_udp"), null, "CrewChiefV4.PCars.PCarsSpotterv2",
+        public static GameDefinition pCarsNetwork = new GameDefinition(GameEnum.PCARS_NETWORK, "pcars_udp", null, "CrewChiefV4.PCars.PCarsSpotterv2",
             null, null, null);
-        public static GameDefinition rFactor1 = new GameDefinition(GameEnum.RF1, Configuration.getUIString("rfactor1"), "rFactor", "CrewChiefV4.rFactor1.RF1Spotter",
+        public static GameDefinition rFactor1 = new GameDefinition(GameEnum.RF1, "rfactor1", "rFactor", "CrewChiefV4.rFactor1.RF1Spotter",
             "rf1_launch_exe", "rf1_launch_params", "launch_rfactor1");
-        public static GameDefinition gameStockCar = new GameDefinition(GameEnum.RF1, Configuration.getUIString("gamestockcar"), "GSC", "CrewChiefV4.rFactor1.RF1Spotter",
+        public static GameDefinition gameStockCar = new GameDefinition(GameEnum.RF1, "gamestockcar", "GSC", "CrewChiefV4.rFactor1.RF1Spotter",
             "gsc_launch_exe", "gsc_launch_params", "launch_gsc");
-        public static GameDefinition automobilista = new GameDefinition(GameEnum.RF1, Configuration.getUIString("automobilista"), "AMS", "CrewChiefV4.rFactor1.RF1Spotter",
+        public static GameDefinition automobilista = new GameDefinition(GameEnum.RF1, "automobilista", "AMS", "CrewChiefV4.rFactor1.RF1Spotter",
             "ams_launch_exe", "ams_launch_params", "launch_ams");
-        public static GameDefinition marcas = new GameDefinition(GameEnum.RF1, Configuration.getUIString("marcas"), "MARCAS", "CrewChiefV4.rFactor1.RF1Spotter",
+        public static GameDefinition marcas = new GameDefinition(GameEnum.RF1, "marcas", "MARCAS", "CrewChiefV4.rFactor1.RF1Spotter",
             "marcas_launch_exe", "marcas_launch_params", "launch_marcas");
-        public static GameDefinition ftruck = new GameDefinition(GameEnum.RF1, Configuration.getUIString("ftruck"), "FTRUCK", "CrewChiefV4.rFactor1.RF1Spotter",
+        public static GameDefinition ftruck = new GameDefinition(GameEnum.RF1, "ftruck", "FTRUCK", "CrewChiefV4.rFactor1.RF1Spotter",
             "ftruck_launch_exe", "ftruck_launch_params", "launch_ftruck");
-        public static GameDefinition assetto64Bit = new GameDefinition(GameEnum.ASSETTO_64BIT, Configuration.getUIString("assetto_64_bit"), "acs", "CrewChiefV4.assetto.ACSSpotter",
+        public static GameDefinition assetto64Bit = new GameDefinition(GameEnum.ASSETTO_64BIT, "assetto_64_bit", "acs", "CrewChiefV4.assetto.ACSSpotter",
             "acs_launch_exe", "acs_launch_params", "launch_acs");
-        public static GameDefinition assetto32Bit = new GameDefinition(GameEnum.ASSETTO_32BIT, Configuration.getUIString("assetto_32_bit"), "acs_x86", "CrewChiefV4.assetto.ACSSpotter",
+        public static GameDefinition assetto32Bit = new GameDefinition(GameEnum.ASSETTO_32BIT, "assetto_32_bit", "acs_x86", "CrewChiefV4.assetto.ACSSpotter",
             "acs_launch_exe", "acs_launch_params", "launch_acs");
-        public static GameDefinition rfactor2_64bit = new GameDefinition(GameEnum.RF2_64BIT, Configuration.getUIString("rfactor2_64_bit"), "rFactor2", "CrewChiefV4.rFactor2.RF2Spotter",
+        public static GameDefinition rfactor2_64bit = new GameDefinition(GameEnum.RF2_64BIT, "rfactor2_64_bit", "rFactor2", "CrewChiefV4.rFactor2.RF2Spotter",
             "rf2_launch_exe", "rf2_launch_params", "launch_rfactor2");
 
 
@@ -82,22 +82,25 @@ namespace CrewChiefV4
             {
                 names.Add(def.friendlyName);
             }
+            names.Sort();
             return names.ToArray();
         }
 
         public GameEnum gameEnum;
         public String friendlyName;
+        public String lookupName;
         public String processName;
         public String spotterName;
         public String gameStartCommandProperty;
         public String gameStartCommandOptionsProperty;
         public String gameStartEnabledProperty;
 
-        public GameDefinition(GameEnum gameEnum, String friendlyName, String processName, 
+        public GameDefinition(GameEnum gameEnum, String lookupName, String processName, 
             String spotterName, String gameStartCommandProperty, String gameStartCommandOptionsProperty, String gameStartEnabledProperty)
         {
             this.gameEnum = gameEnum;
-            this.friendlyName = friendlyName;
+            this.lookupName = lookupName;
+            this.friendlyName = Configuration.getUIString(lookupName);
             this.processName = processName;            
             this.spotterName = spotterName;
             this.gameStartCommandProperty = gameStartCommandProperty;
