717068ce327b30a913e2b11f893f23c2eda200f3
Sparten <sportcorp@hotmail.com>
2017-03-22 19:55:43 +0100

    updated shared memory and added pitwindow.

--- CrewChiefV4/ACS/ACSData.cs
+++ CrewChiefV4/ACS/ACSData.cs
@@ -183,6 +183,7 @@ namespace CrewChiefV4.assetto
             public int isInPitLane;
 
             public float surfaceGrip;
+            public int MandatoryPitDone; 
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 4, CharSet = CharSet.Unicode)]
@@ -246,6 +247,11 @@ namespace CrewChiefV4.assetto
             public float ersMaxJ;
             public int isTimedRace;
             public int hasExtraLap;
+            [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 33)]
+            public String carSkin;
+            public int reversedGridPositions;
+            public int PitWindowStart;
+            public int PitWindowEnd;
             
         }
 
