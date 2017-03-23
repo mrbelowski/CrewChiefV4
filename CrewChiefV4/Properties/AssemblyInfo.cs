6d0569983aa7bae93442cc617db9abf07ed51e0e
mrbelowski <jim.britton@yahoo.co.uk>
2017-03-23 18:27:19 +0000

    build stuff

--- CrewChiefV4/Properties/AssemblyInfo.cs
+++ CrewChiefV4/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.6.3.0")]
+[assembly: AssemblyVersion("4.6.3.1")]
 
 [assembly: AssemblyFileVersion("1.0.0.0")]
