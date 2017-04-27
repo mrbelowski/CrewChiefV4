a62902c6771c5f41de7d01b408e52c362a565727
mrbelowski <jim.britton@yahoo.co.uk>
2017-04-27 18:28:40 +0100

    clean up AC placeholder LapData elements that get added to opponent data at the start of a session

--- CrewChiefV4/ACS/ACSGameStateMapper.cs
+++ CrewChiefV4/ACS/ACSGameStateMapper.cs
@@ -2027,8 +2027,18 @@ namespace CrewChiefV4.assetto
                 {
                     if (opponentData.OpponentLapData.Count > 0)
                     {
-                        opponentData.CompleteLapWithProvidedLapTime(leaderBoardPosition, sessionRunningTime, lastLapTime,
-                            lapIsValid && validSpeed, false, trackTempreture, airTemperature, sessionLengthIsTime, sessionTimeRemaining, trackNumberOfSectors);
+                        // special case here: if there's only 1 lap in the list, and it's marked as an in-lap, we don't have a laptime, remove it.
+                        // This is because we might have created a new LapData entry to hold a partially completed in-lap if we join mid-session, but
+                        // this also results in each opponent having a spurious 'emtpy' LapData element.
+                        if (opponentData.OpponentLapData.Count == 1 && opponentData.OpponentLapData[0].InLap && lastLapTime == 0)
+                        {
+                            opponentData.OpponentLapData.Clear();
+                        }
+                        else
+                        {
+                            opponentData.CompleteLapWithProvidedLapTime(leaderBoardPosition, sessionRunningTime, lastLapTime,
+                                lapIsValid && validSpeed, false, trackTempreture, airTemperature, sessionLengthIsTime, sessionTimeRemaining, trackNumberOfSectors);
+                        }
                     }
 
                     opponentData.StartNewLap(completedLaps + 1, leaderBoardPosition, isInPits, sessionRunningTime, false, trackTempreture, airTemperature);
