d583214ebb653c472429df07f20adfda31a32b19
TheIronWolfModding <vleonavicius@hotmail.com>
2017-08-05 11:09:15 -0700

    Fix NRE on messageFolders == null

--- CrewChiefV4/Audio/AudioPlayer.cs
+++ CrewChiefV4/Audio/AudioPlayer.cs
@@ -569,18 +569,25 @@ namespace CrewChiefV4.Audio
                                             keyBleepIn = "short_start_bleep";
                                         }
 
-                                        Console.WriteLine("INSERTED FAKE BEEP OUT/IN");
-                                        bool withBleepOut = true;
-                                        if (withBleepOut)
+                                        if (queuedMessage.messageFolders != null)
                                         {
-                                            // insert bleep out/in
-                                            queuedMessage.messageFolders.Insert(0, keyBleepOut);
-                                            // would be nice to have some slight random silence here
-                                            queuedMessage.messageFolders.Insert(1, keyBleepIn);
+                                            Console.WriteLine("INSERTED FAKE BEEP OUT/IN");
+                                            bool withBleepOut = true;
+                                            if (withBleepOut)
+                                            {
+                                                // insert bleep out/in
+                                                queuedMessage.messageFolders.Insert(0, keyBleepOut);
+                                                // would be nice to have some slight random silence here
+                                                queuedMessage.messageFolders.Insert(1, keyBleepIn);
+                                            }
+                                            else
+                                            {
+                                                queuedMessage.messageFolders.Insert(0, keyBleepIn);
+                                            }
                                         }
                                         else
                                         {
-                                            queuedMessage.messageFolders.Insert(0, keyBleepIn);
+                                            Console.WriteLine("COULD NOT INSERT FAKE BEEP OUT/IN due to messageFolders null");
                                         }
                                     }
 
