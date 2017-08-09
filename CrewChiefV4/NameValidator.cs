42c452e6ab86803b96f4a7fbfd4acf203eb78271
Jim Britton <jim.britton@yahoo.co.uk>
2017-08-09 13:38:33 +0100

    Update NameValidator.cs

--- CrewChiefV4/NameValidator.cs
+++ CrewChiefV4/NameValidator.cs
@@ -11,8 +11,8 @@ namespace CrewChiefV4
 
         // TODO: add more undeserving shitbags to this list as and when they crawl out the woodwork
          private static String[] wankers = new String[] { "Mr.SisterFister", "BigSilverHotdog", 
-             "Paul Hance", "Aline Senna", "Giuseppe Sangalli", "Patrick Förster", "Chris Iwaski", 
-             /* "Valentino Rossi" TODO add Rossi next time he behaves like a spoiled 5 year old */};
+             "Paul Hance", "Aline Senna", "Giuseppe Sangalli", "Patrick Förster", "Chris Iwaski", "gazman"
+             /* ",Valentino Rossi" TODO add Rossi next time he behaves like a spoiled 5 year old */};
  
           public static Boolean validateName(String name)
           {
