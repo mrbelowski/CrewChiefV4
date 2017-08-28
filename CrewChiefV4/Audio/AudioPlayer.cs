5a8d7c14bcaacd9293cdadc02dd66805b8793a23
mrbelowski <jim.britton@yahoo.co.uk>
2017-08-28 15:21:39 +0100

    prevent radio check messages being purged on session start

--- CrewChiefV4/Audio/AudioPlayer.cs
+++ CrewChiefV4/Audio/AudioPlayer.cs
@@ -910,26 +910,31 @@ namespace CrewChiefV4.Audio
 
         public int purgeQueues()
         {
+            return purgeQueue(queuedClips) + purgeQueue(immediateClips);
+        }
+
+        private int purgeQueue(OrderedDictionary queue)
+        {
             int purged = 0;
-            lock (queuedClips)
+            lock (queue)
             {
-                ArrayList keysToPurge = new ArrayList(queuedClips.Keys);
+                ArrayList keysToPurge = new ArrayList(queue.Keys);
                 foreach (String keyStr in keysToPurge)
                 {
-                    if (!keyStr.Contains(SessionEndMessages.sessionEndMessageIdentifier) && queuedClips.Contains(keyStr))
+                    try
                     {
-                        queuedClips.Remove(keyStr);
-                        purged++;
+                        if (!keyStr.Contains(SessionEndMessages.sessionEndMessageIdentifier) &&
+                            !keyStr.Contains(SmokeTest.SMOKE_TEST))
+                        {
+                            queue.Remove(keyStr);
+                            purged++;
+                        }
                     }
-                    else
+                    catch (Exception)
                     {
-                        Console.WriteLine("Not purging session end message");
+                        // ignore - not sure why I'm try-catching here :)
                     }
                 }
-            }
-            lock (immediateClips)
-            {
-                immediateClips.Clear();
             }
             return purged;
         }
