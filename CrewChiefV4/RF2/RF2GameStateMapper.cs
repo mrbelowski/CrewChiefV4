1b4a69f030b7017457daf376dbc2a85321b3232f
TheIronWolfModding <vleonavicius@hotmail.com>
2017-09-23 13:21:22 -0700

    rF2: Add some code to detect DRS state; detect tracks with no DRS zones; extend car classes to specify if they're DRS capable and DRS detection range.

--- CrewChiefV4/RF2/RF2GameStateMapper.cs
+++ CrewChiefV4/RF2/RF2GameStateMapper.cs
@@ -57,6 +57,9 @@ namespace CrewChiefV4.rFactor2
         private double lastPlayerTelemetryET = -1.0;
         private double lastScoringET = -1.0;
 
+        // True if it looks like track has no DRS zones defined.
+        private bool detectedTrackNoDRSZones = false;
+
         public RF2GameStateMapper()
         {
             this.tyreWearThresholds.Add(new CornerData.EnumWithThresholds(TyreCondition.NEW, -10000.0f, this.scrubbedTyreWearPercent));
@@ -445,11 +448,13 @@ namespace CrewChiefV4.rFactor2
                 csd.IsNewSession = true;
             }
 
-            // Do not use previous game state if this is the new session.
             if (csd.IsNewSession)
             {
+                // Do not use previous game state if this is the new session.
                 pgs = null;
+
                 GlobalBehaviourSettings.UpdateFromCarClass(cgs.carClass);
+                this.detectedTrackNoDRSZones = false;
             }
 
             // Restore cumulative data.
@@ -847,6 +852,34 @@ namespace CrewChiefV4.rFactor2
                     (float)shared.scoring.mScoringInfo.mAmbientTemp, (float)shared.scoring.mScoringInfo.mTrackTemp, (float)shared.scoring.mScoringInfo.mRaining,
                     (float)Math.Sqrt((double)(shared.scoring.mScoringInfo.mWind.x * shared.scoring.mScoringInfo.mWind.x + shared.scoring.mScoringInfo.mWind.y * shared.scoring.mScoringInfo.mWind.y + shared.scoring.mScoringInfo.mWind.z * shared.scoring.mScoringInfo.mWind.z)), 0, 0, 0);
             }
+
+            // --------------------------------
+            // DRS data
+            cgs.OvertakingAids.DrsAvailable = playerTelemetry.mRearFlapLegalStatus == (int)rFactor2Constants.rF2RearFlapLegalStatus.Alllowed;
+
+            // Many of rF2 tracks have no DRS zones defined.  One of the symptoms is DRS alloweved immediately on race start.
+            // Disallow DRS messages in such case.
+            if (!this.detectedTrackNoDRSZones
+                && csd.CompletedLaps == 0 
+                && csd.SessionRunningTime > 10 
+                && cgs.OvertakingAids.DrsAvailable)
+            {
+                this.detectedTrackNoDRSZones = true;
+                if (cgs.carClass.isDRSCapable)
+                    Console.WriteLine("Track has no valid DRS zones defined, disabling DRS messages.");
+            }
+
+            cgs.OvertakingAids.DrsEngaged = playerTelemetry.mRearFlapActivated == 1;
+
+            if (cgs.SessionData.SessionPhase != SessionPhase.FullCourseYellow)
+            {
+                // Doesn't look like game is providing info on when DRS is actually enabled in race, so guess.
+                cgs.OvertakingAids.DrsEnabled = cgs.carClass.isDRSCapable
+                    && csd.CompletedLaps > 2  // Hack of course.
+                    && !this.detectedTrackNoDRSZones;
+            }
+
+            cgs.OvertakingAids.DrsRange = cgs.carClass.DRSRange;
 
             // --------------------------------
             // opponent data
