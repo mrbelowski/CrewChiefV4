6d23a73d6efcfee387ca8fc7ee3fe7b75138a56c
TheIronWolfModding <vleonavicius@hotmail.com>
2017-09-26 09:50:12 -0700

    rF2: More session end fixes.

--- CrewChiefV4/RF2/RF2GameStateMapper.cs
+++ CrewChiefV4/RF2/RF2GameStateMapper.cs
@@ -253,8 +253,8 @@ namespace CrewChiefV4.rFactor2
                             var playerVehCapture = shared.extended.mSessionTransitionCapture.mScoringVehicles[playerVehIdx];
                             if (pgs.SessionData.Position != playerVehCapture.mPlace)
                             {
-                                Console.WriteLine(string.Format("Abrupt Session End: player position was updated after session end, updating from pos {0} to: {1}."),
-                                    pgs.SessionData.Position, playerVehCapture.mPlace);
+                                Console.WriteLine(string.Format("Abrupt Session End: player position was updated after session end, updating from pos {0} to: {1}.",
+                                    pgs.SessionData.Position, playerVehCapture.mPlace));
                                 pgs.SessionData.Position = playerVehCapture.mPlace;
                             }
                         }
@@ -282,6 +282,7 @@ namespace CrewChiefV4.rFactor2
                 {
                     pgs.SessionData.SessionType = SessionType.Unavailable;
                     pgs.SessionData.SessionPhase = SessionPhase.Unavailable;
+                    pgs.SessionData.AbruptSessionEndDetected = false;
                 }
 
                 return pgs;
