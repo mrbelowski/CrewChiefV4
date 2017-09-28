9c8e41e91c39728aaaf543ec2d7691df9ebf5bfc
mrbelowski <jim.britton@yahoo.co.uk>
2017-09-28 22:01:25 +0100

    restore laptimeprevious from previous gamestate

--- CrewChiefV4/R3E/R3EGameStateMapper.cs
+++ CrewChiefV4/R3E/R3EGameStateMapper.cs
@@ -383,6 +383,7 @@ namespace CrewChiefV4.RaceRoom
                     currentGameState.SessionData.LastSector1Time = previousGameState.SessionData.LastSector1Time;
                     currentGameState.SessionData.LastSector2Time = previousGameState.SessionData.LastSector2Time;
                     currentGameState.SessionData.LastSector3Time = previousGameState.SessionData.LastSector3Time;
+                    currentGameState.SessionData.LapTimePrevious = previousGameState.SessionData.LapTimePrevious;
                     currentGameState.SessionData.PlayerBestSector1Time = previousGameState.SessionData.PlayerBestSector1Time;
                     currentGameState.SessionData.PlayerBestSector2Time = previousGameState.SessionData.PlayerBestSector2Time;
                     currentGameState.SessionData.PlayerBestSector3Time = previousGameState.SessionData.PlayerBestSector3Time;
