8ae2a8b84bd42b1ff6c2dfad1cd9abcff250b138
Sparten <sportcorp@hotmail.com>
2017-10-05 08:39:19 +0200

    more updates and fixes

--- CrewChiefV4/iRacing/Sector.cs
+++ CrewChiefV4/iRacing/Sector.cs
@@ -10,7 +10,7 @@ namespace CrewChiefV4.iRacing
     {
         public Sector()
         {
-            SectorTime = new Laptime(-1);
+            SectorTime = new Laptime(0);
         }
         public int Number { get; set; }
         public float StartPercentage { get; set; }
