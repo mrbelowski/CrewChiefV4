a6f74c2a40e84791418a7e1ac819a3c0d8a0c5bb
mrbelowski <jim.britton@yahoo.co.uk>
2017-10-09 21:53:57 +0100

    WIP additions to support voice commands triggering keyboard press macros

---
+++ CrewChiefV4/commands/CommandMacro.cs
@@ -0,0 +1,63 @@
+﻿using CrewChiefV4.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrewChiefV4.commands
+{
+    public class CommandMacro
+    {
+        AudioPlayer audioPlayer;
+        MacroItem[] items;
+        String confirmationMessage;
+        public CommandMacro(AudioPlayer audioPlayer, String confirmationMessage, params MacroItem[] items)
+        {
+            this.audioPlayer = audioPlayer;
+            this.confirmationMessage = confirmationMessage;
+            this.items = items;
+        }
+        public void execute()
+        {
+            // blocking...
+            foreach (MacroItem item in items)
+            {
+                if (item.macroItemType == MacroItem.MacroItemType.KEYPRESS)
+                {
+                    KeyPresser.SendKeyPress(item.keycode);
+                }
+                else if (item.macroItemType == MacroItem.MacroItemType.KEYPRESS)
+                {
+                    Thread.Sleep(item.pauseMillis);
+                }
+            }
+            audioPlayer.playMessageImmediately(new QueuedMessage(confirmationMessage, 0, null));
+        }
+    }
+
+    public class MacroItem
+    {
+        public CrewChiefV4.commands.KeyPresser.KeyCode keycode;
+        public int pauseMillis;
+        public MacroItemType macroItemType;
+
+        public MacroItem(CrewChiefV4.commands.KeyPresser.KeyCode keycode)
+        {
+            this.keycode = keycode;
+            this.macroItemType = MacroItemType.KEYPRESS;
+        }
+
+        public MacroItem(int pauseMillis)
+        {
+            this.pauseMillis = pauseMillis;
+            this.macroItemType = MacroItemType.PAUSE;
+        }
+
+        public enum MacroItemType
+        {
+            KEYPRESS, PAUSE
+        }
+    }
+}
---
+++ CrewChiefV4/commands/MacroManager.cs
@@ -0,0 +1,34 @@
+﻿using CrewChiefV4.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewChiefV4.commands
+{
+    class MacroManager
+    {
+        // initialise some static test hack to see if this shit hangs together. This is called immediately after initialising 
+        // the speech recogniser in MainWindow.
+        public static void initialise(AudioPlayer audioPlayer, SpeechRecogniser speechRecogniser)
+        {
+            Dictionary<string, CommandMacro> voiceTriggeredMacros = new Dictionary<string, CommandMacro>();
+
+            // the test command at the bottom of the speech recogniser config file
+            String testCommandSpeechRecogniserPhrase = "MACRO_TEST";    
+
+            // quick n dirty smoke test - press some keys with pauses - these could be assigned to open pit menu, go right, confirm, whatever
+            MacroItem[] r3eSelectNextPitItem = new MacroItem[] { 
+                new MacroItem(CrewChiefV4.commands.KeyPresser.KeyCode.KEY_Q),
+                new MacroItem(100),
+                new MacroItem(CrewChiefV4.commands.KeyPresser.KeyCode.KEY_D),
+                new MacroItem(100),
+                new MacroItem(CrewChiefV4.commands.KeyPresser.KeyCode.KEY_E)
+            };
+
+            voiceTriggeredMacros.Add(testCommandSpeechRecogniserPhrase, new CommandMacro(audioPlayer, AudioPlayer.folderAcknowlegeOK, r3eSelectNextPitItem));
+            speechRecogniser.loadMacroVoiceTriggers(voiceTriggeredMacros);
+        }
+    }
+}
