ef1ed153bf842a137df2a6da0dacf4caedd4c4da
mrbelowski <jim.britton@yahoo.co.uk>
2017-11-14 08:33:23 +0000

    properly restore previous game state's nextDeltaPoint in all mappers

--- CrewChiefV4/PCars2/PCars2GameStateMapper.cs
+++ CrewChiefV4/PCars2/PCars2GameStateMapper.cs
@@ -496,7 +496,7 @@ namespace CrewChiefV4.PCars2
 
                     currentGameState.SessionData.DeltaTime.deltaPoints = previousGameState.SessionData.DeltaTime.deltaPoints;
                     currentGameState.SessionData.DeltaTime.currentDeltaPoint = previousGameState.SessionData.DeltaTime.currentDeltaPoint;
-                    currentGameState.SessionData.DeltaTime.nextDeltaPoint = previousGameState.SessionData.DeltaTime.currentDeltaPoint;
+                    currentGameState.SessionData.DeltaTime.nextDeltaPoint = previousGameState.SessionData.DeltaTime.nextDeltaPoint;
                     currentGameState.SessionData.DeltaTime.lapsCompleted = previousGameState.SessionData.DeltaTime.lapsCompleted;
                     currentGameState.SessionData.DeltaTime.totalDistanceTravelled = previousGameState.SessionData.DeltaTime.totalDistanceTravelled;
                     currentGameState.SessionData.DeltaTime.trackLength = previousGameState.SessionData.DeltaTime.trackLength;
