c9864d2e7cde4eb91fc3f911fad631c2f8bb0524
Sparten <sportcorp@hotmail.com>
2017-11-15 11:13:39 +0100

    Small change to detect oval racing

--- CrewChiefV4/iRacing/iRacingGameStateMapper.cs
+++ CrewChiefV4/iRacing/iRacingGameStateMapper.cs
@@ -216,7 +216,7 @@ namespace CrewChiefV4.iRacing
 
                         TrackDataContainer tdc = TrackData.TRACK_LANDMARKS_DATA.getTrackDataForTrackName(shared.SessionData.Track.CodeName, currentGameState.SessionData.TrackDefinition.trackLength);
                         currentGameState.SessionData.TrackDefinition.trackLandmarks = tdc.trackLandmarks;
-                        currentGameState.SessionData.TrackDefinition.isOval = tdc.isOval;
+                        currentGameState.SessionData.TrackDefinition.isOval = tdc.isOval || shared.SessionData.Track.IsOval;
                         currentGameState.SessionData.TrackDefinition.setGapPoints();
                         GlobalBehaviourSettings.UpdateFromTrackDefinition(currentGameState.SessionData.TrackDefinition);
 
