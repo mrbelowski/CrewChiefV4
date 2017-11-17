3c490a5ab1ee9d55e5b9c3e3579ac2c17a563091
Jim Britton <jim.britton@yahoo.co.uk>
2017-11-17 16:13:57 +0000

    wsBcBAABCAAQBQJaDwrFCRBK7hj4Ov3rIwAAdHIIAHbAmlHdyBtW+1A3dZp68Kyi  MdLSkupHOW1UjNqlv3o7nAc62x1vTeCPGQ5LGMZKNHdOVEZ4uZEJHYq5q4LR5h4w  latwKsLO+5x3kXXKbjxshuUc9ggSjBAFjtUNmCNnFSd8sqXV6ctPOFWycVkVP3ib  KAPLSP4oWYETII83J+WTFTfiOmnIVTtOQ9ka7L/wWCtVm1CKjD783CgkNVtTVERX  db6TXZsIVCr8QdRRAAAE5wDE8OEvvrIcaQGG0ie/0OXqBoOrCOzc5HsayBie0xF5  uWN+384amrQeu1/UH7Yo9A0DxwniwmNEOQ4mJpIAafzWJFnFc/rxAWtR54W2+pQ=  =wlFt  -----END PGP SIGNATURE-----
    Naudio playback (#180)
    
    * quick n dirty naudio implementation for all playback
    
    * a couple of minor things
    
    * layout fix
    
    * added missing UI text
    
    * dispose naudio BGP when audio player is disposed
    
    * removed Micha spotter
    
    * new version of Micha's spotter
    
    * filtered Micha's spotter sounds

---
+++ CrewChiefV4/Audio/NAudioBackgroundPlayer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewChiefV4.Audio
+{
+    class NAudioBackgroundPlayer : BackgroundPlayer
+    {
+        private Boolean playing = false;
+
+        private NAudio.Wave.WaveOutEvent waveOut = null;
+        private NAudio.Wave.WaveFileReader reader = null;
+
+        private TimeSpan backgroundLength = TimeSpan.Zero;
+
+        public NAudioBackgroundPlayer(String backgroundFilesPath, String defaultBackgroundSound)
+        {
+            this.backgroundFilesPath = backgroundFilesPath;
+            this.defaultBackgroundSound = defaultBackgroundSound;
+        }
+
+        public override void mute(bool doMute)
+        {
+            this.muted = doMute;
+        }
+
+        public override void play()
+        {
+            if (playing || muted)
+            {
+                return;
+            }
+            if (!initialised)
+            {
+                initialise(this.defaultBackgroundSound);
+            }
+            int backgroundOffset = Utilities.random.Next(0, (int) backgroundLength.TotalSeconds - backgroundLeadout);
+            reader.CurrentTime = TimeSpan.FromSeconds(backgroundOffset);
+            long samples = reader.SampleCount;
+
+            NAudio.Wave.SampleProviders.SampleChannel sampleChannel = new NAudio.Wave.SampleProviders.SampleChannel(reader);
+            sampleChannel.Volume = getBackgroundVolume();
+            this.waveOut.DeviceNumber = AudioPlayer.naudioBackgroundPlaybackDeviceId;
+            this.waveOut.Init(new NAudio.Wave.SampleProviders.SampleToWaveProvider(sampleChannel));
+            this.waveOut.Play();
+        }
+
+        public override void stop()
+        {
+            if (initialised)
+            {
+                waveOut.Stop();
+            }
+        }
+
+        public override void initialise(String initialBackgroundSound)
+        {
+            this.reader = new NAudio.Wave.WaveFileReader(Path.Combine(backgroundFilesPath, initialBackgroundSound));
+            this.waveOut = new NAudio.Wave.WaveOutEvent();
+            backgroundLength = reader.TotalTime;
+            initialised = true;
+        }
+
+        public override void setBackgroundSound(String backgroundSoundName)
+        {
+            if (initialised)
+            {
+                stop();
+                try
+                {
+                    this.reader.Dispose();
+                    this.waveOut.Dispose();
+                }
+                catch (Exception) { }
+            }
+            this.reader = new NAudio.Wave.WaveFileReader(Path.Combine(backgroundFilesPath, backgroundSoundName));
+            this.waveOut = new NAudio.Wave.WaveOutEvent();
+            backgroundLength = reader.TotalTime;
+        }
+
+        public override void dispose()
+        {
+            try
+            {
+                reader.Dispose();
+                waveOut.Dispose();
+            }
+            catch (Exception) { }
+            base.dispose();
+        }
+    }
+}
