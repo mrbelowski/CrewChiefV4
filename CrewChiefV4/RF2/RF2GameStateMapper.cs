25fc768403e9021d89497a3fbc2329ea9343e145
TheIronWolfModding <vleonavicius@hotmail.com>
2017-12-21 13:43:12 -0800

    Prepare for capture of experimental SCR hack variables.

--- CrewChiefV4/RF2/RF2GameStateMapper.cs
+++ CrewChiefV4/RF2/RF2GameStateMapper.cs
@@ -96,7 +96,7 @@ namespace CrewChiefV4.rFactor2
             this.suspensionDamageThresholds.Add(new CornerData.EnumWithThresholds(DamageLevel.DESTROYED, 1.0f, 2.0f));
         }
 
-        private int[] minimumSupportedVersionParts = new int[] { 2, 2, 1, 0 };
+        private int[] minimumSupportedVersionParts = new int[] { 2, 3, 0, 0 };
         public static bool pluginVerified = false;
         private string lastVersionString;
         public void versionCheck(Object memoryMappedFileStruct)
@@ -151,7 +151,7 @@ namespace CrewChiefV4.rFactor2
             {
                 RF2GameStateMapper.pluginVerified = true;
 
-                var msg = "rFactor 2 Shared Memory version: " + versionStr + " 64bit";
+                var msg = "rFactor 2 Shared Memory version: " + versionStr + " 64bit." + (shared.extended.isStockCarRulesPluginHosted != 0 ? "  Stock Car Rules plugin hosted." : "");
                 Console.WriteLine(msg);
             }
         }
