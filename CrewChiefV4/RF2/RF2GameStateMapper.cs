b393b8a688f4aaea349e374e067fcc945261c79e
TheIronWolfModding <vleonavicius@hotmail.com>
2017-12-31 16:51:10 -0800

    rF2: bump plugin version

--- CrewChiefV4/RF2/RF2GameStateMapper.cs
+++ CrewChiefV4/RF2/RF2GameStateMapper.cs
@@ -108,7 +108,7 @@ namespace CrewChiefV4.rFactor2
             this.suspensionDamageThresholds.Add(new CornerData.EnumWithThresholds(DamageLevel.DESTROYED, 1.0f, 2.0f));
         }
 
-        private int[] minimumSupportedVersionParts = new int[] { 2, 3, 1, 1 };
+        private int[] minimumSupportedVersionParts = new int[] { 2, 3, 1, 2 };
         public static bool pluginVerified = false;
         private string lastVersionString;
         public void versionCheck(Object memoryMappedFileStruct)
