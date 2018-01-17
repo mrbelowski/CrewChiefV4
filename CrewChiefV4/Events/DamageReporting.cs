d37767dd98e95c9eafd84f34e511b69675ec7b31
mrbelowski <jim.britton@yahoo.co.uk>
2018-01-17 11:50:15 +0000

    typo

--- CrewChiefV4/Events/DamageReporting.cs
+++ CrewChiefV4/Events/DamageReporting.cs
@@ -60,8 +60,8 @@ namespace CrewChiefV4.Events
         // "the car's in good shape" / "we have no significant damage" etc
         private String folderNoDamageOnAnyComponent = "damage_reporting/no_damage";
 
-        private String folderRolled = "damaged_reporting/rolled";
-        private String folderStoppedUpsideDown = "damaged_reporting/stopped_upside_down";
+        private String folderRolled = "damage_reporting/rolled";
+        private String folderStoppedUpsideDown = "damage_reporting/stopped_upside_down";
 
         private DamageLevel engineDamage;
         private DamageLevel trannyDamage;
