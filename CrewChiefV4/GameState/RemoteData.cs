5d084ecf5fc19109c8cfa6129669ec9a851dc4b0
Jim Britton <jim.britton@yahoo.co.uk>
2018-03-29 18:03:14 +0100

    more wip farting about

---
+++ CrewChiefV4/GameState/RemoteData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewChiefV4.GameState
+{
+    // value object to hold all remote data gather from the various remote readers
+    public class RemoteData
+    {
+        public ACKMRData acKMRData = new ACKMRData();
+        public RestData restData = new RestData();
+    }
+
+    public class ACKMRData
+    {
+
+    }
+
+    public class RestData
+    {
+        public String pacenotesSet;
+        public Boolean pacenotesEnabled = false;
+    }
+}
---
+++ CrewChiefV4/RestDataReader.cs
@@ -0,0 +1,80 @@
+﻿using CrewChiefV4.GameState;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Web;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+
+namespace CrewChiefV4
+{
+    [ServiceContract]
+    public class RestService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate="test")]
+        public String test()
+        {
+            //WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.OK;
+            return "hello";
+        }
+    }
+
+    public class RestController
+    {
+        private ServiceHost host;
+        
+        public void start()
+        {
+            host = new ServiceHost(typeof(RestService), new Uri("http://localhost:8080/"));
+            var endpoint = host.AddServiceEndpoint(typeof(RestService), new WebHttpBinding(), "service");
+            endpoint.EndpointBehaviors.Add(new WebHttpBehavior());
+            host.Open();
+        }
+
+        public void stop()
+        {
+            host.Close();
+        }
+    }
+    public class RestDataReader : RemoteDataReader
+    {
+        System.ServiceModel.ServiceHost serviceHost;
+        private Boolean pacenotesEnabled = false;
+        private String latestRequestedPacenotesSet;
+
+        // set to true when a Rest call is made, then false once that data has been read by the main loop
+        private Boolean hasNewData = false;
+
+        private RestController controller;
+
+        public override Boolean autoStart()
+        {
+            return true;
+        }
+
+        public override void activate(object activationData)
+        {
+            if (controller != null)
+            {
+                controller.stop();
+            }
+            controller = new RestController();
+            controller.start();
+            base.activate(activationData);
+        }
+
+        public override RemoteData getRemoteDataInternal(RemoteData remoteData, Object rawGameData)
+        {
+            if (hasNewData)
+            {
+                // move received data into the remoteData object
+                hasNewData = false;
+            }
+            return remoteData;
+        }
+    }
+}
