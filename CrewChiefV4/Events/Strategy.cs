4985be6f7a77e78054538c10fc3ccbc88a20a9ed
mrbelowski <jim.britton@yahoo.co.uk>
2018-06-06 21:31:35 +0100

    strategy bugfixes

--- CrewChiefV4/Events/Strategy.cs
+++ CrewChiefV4/Events/Strategy.cs
@@ -57,6 +57,7 @@ namespace CrewChiefV4.Events
         private static Dictionary<String, float> opponentsTimeLostForStop = new Dictionary<string, float>();
 
         private Boolean isTimingPracticeStop = false;
+        private int lapsCompletedAtStartOfTimedStop = -1;
         private float gameTimeAtPracticeStopTimerStart = -1;
         private float s3AndS1TimeOnStop = -1;
 
@@ -106,6 +107,7 @@ namespace CrewChiefV4.Events
             timeOpponentStops = true;
             opponentsInS2.Clear();
             Strategy.opponentsWhoWillExitCloseBehind.Clear();
+            lapsCompletedAtStartOfTimedStop = -1;
             Strategy.opponentsWhoWillExitCloseInFront.Clear();
         }
                 
@@ -127,14 +129,17 @@ namespace CrewChiefV4.Events
             {
                 if (currentGameState.SessionData.SectorNumber == 3 && previousGameState.SessionData.SectorNumber == 2)
                 {
-                    gameTimeAtPracticeStopTimerStart = currentGameState.SessionData.SessionRunningTime;                    
+                    gameTimeAtPracticeStopTimerStart = currentGameState.SessionData.SessionRunningTime;
+                    lapsCompletedAtStartOfTimedStop = currentGameState.SessionData.CompletedLaps;
                 }
-                else if (currentGameState.SessionData.SectorNumber == 2 && previousGameState.SessionData.SectorNumber == 1)
+                else if (currentGameState.SessionData.SectorNumber == 2 && previousGameState.SessionData.SectorNumber == 1 && 
+                    currentGameState.SessionData.CompletedLaps == lapsCompletedAtStartOfTimedStop + 1)
                 {
                     s3AndS1TimeOnStop = currentGameState.SessionData.SessionRunningTime - gameTimeAtPracticeStopTimerStart;
                     Strategy.playerTimeLostForStop = s3AndS1TimeOnStop - (currentGameState.SessionData.PlayerBestLapSector3Time + currentGameState.SessionData.PlayerBestLapSector1Time);
                     gameTimeAtPracticeStopTimerStart = -1;
                     isTimingPracticeStop = false;
+                    lapsCompletedAtStartOfTimedStop = -1;
                     Strategy.carClassForLastPitstopTiming = currentGameState.carClass;
                     Strategy.trackNameForLastPitstopTiming = currentGameState.SessionData.TrackDefinition.name;
                     
@@ -143,6 +148,13 @@ namespace CrewChiefV4.Events
                         MessageContents(folderPitStopCostsUsAbout,
                         TimeSpanWrapper.FromSeconds(Strategy.playerTimeLostForStop, Precision.SECONDS)),
                         0, this));
+                }
+                else if (currentGameState.SessionData.CompletedLaps > lapsCompletedAtStartOfTimedStop + 1)
+                {
+                    // something's gone very wrong here
+                    gameTimeAtPracticeStopTimerStart = -1;
+                    isTimingPracticeStop = false;
+                    lapsCompletedAtStartOfTimedStop = -1;
                 }
                 // nothing else to do unless we're in race mode
             }
