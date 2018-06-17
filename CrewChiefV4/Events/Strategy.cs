9d6ad2570a131abfac90caae96fc246debb7a20d
mrbelowski <jim.britton@yahoo.co.uk>
2018-06-17 18:04:18 +0100

    re-enable opponent pit exit prediction for iracing, when we've set a benchmark

--- CrewChiefV4/Events/Strategy.cs
+++ CrewChiefV4/Events/Strategy.cs
@@ -341,9 +341,8 @@ namespace CrewChiefV4.Events
 
 
                 //--------------------------------------
-                // opponent pit exit position estimation - disable this for iRacing
-                if (CrewChief.gameDefinition.gameEnum != GameEnum.IRACING &&
-                    warnAboutOpponentsExitingCloseToPlayer && currentGameState.Now > nextOpponentFinalSectorTimingCheckDue)
+                // opponent pit exit position estimation
+                if (warnAboutOpponentsExitingCloseToPlayer && currentGameState.Now > nextOpponentFinalSectorTimingCheckDue)
                 {
                     float expectedPlayerTimeLoss = -1;
                     DateTime nowMinusExpectedLoss = DateTime.MinValue;
