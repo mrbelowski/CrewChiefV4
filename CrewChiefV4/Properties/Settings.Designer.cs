55ec2f123e7a1608691f9f4c2e5d63eb57d47acb
mrbelowski <jim.britton@yahoo.co.uk>
2018-09-20 13:39:15 +0100

    pcars2 spotter tweaks - only check car class every few seconds; use pcars2 specific car length (5 metres)

--- CrewChiefV4/Properties/Settings.Designer.cs
+++ CrewChiefV4/Properties/Settings.Designer.cs
@@ -3502,5 +3502,17 @@ namespace CrewChiefV4.Properties {
                 this["f1_2018_spotter_car_length"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("5")]
+        public float pcars2_spotter_car_length {
+            get {
+                return ((float)(this["pcars2_spotter_car_length"]));
+            }
+            set {
+                this["pcars2_spotter_car_length"] = value;
+            }
+        }
     }
 }
